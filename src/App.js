acb7047bd7e519544eb7fc3f47bc4a698191bff9
Aryan <aryanbaghel040@gmail.com>
2023-12-16 00:53:06 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,195 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Fragment, useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
+  const [oldPayment, setOldPayment] = useState("");
+  const [paidRecently, setPaidRecently] = useState("");
+  const [date, setDate] = useState("");
+  const [nag, setNag] = useState("");
+  const [amountPerNag, setAmountPerNag] = useState("");
+  const [oldEmpty, setOldEmpty] = useState("");
+  const [emptyDate, setEmptyDate] = useState("");
+  const [recentEmpty, setRecentEmpty] = useState("");
+
+  const [output, setOutput] = useState("");
+
+  const resetHandler = (e) => {
+    e.preventDefault();
+
+    setOldPayment("");
+    setPaidRecently("");
+    setDate("");
+    setNag("");
+    setAmountPerNag("");
+    setOldEmpty("");
+    setEmptyDate("");
+    setRecentEmpty("");
+    setOutput("");
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const outputMessage = `
+      Date = ${new Date(date).toDateString()}
+
+      Old Payment = ${oldPayment}
+      Paid Recently = ${paidRecently}
+
+      -------------------------
+
+      Balance = ${Number(oldPayment - paidRecently)}
+      ${nag} Nag = ${Number(nag * amountPerNag)}
+
+      -------------------------
+
+      Total Amount Remaining = ${
+        Number(oldPayment - paidRecently) + Number(nag * amountPerNag)
+      }
+
+      
+      -------------------------
+      -------------------------
+
+      Old Empty = ${oldEmpty}
+      (${new Date(emptyDate).toDateString()}) Empty = ${recentEmpty}
+
+      -------------------------
+
+      Total Empty Remaining = ${Number(oldEmpty) + Number(recentEmpty)}
+      `;
+
+    setOutput(outputMessage);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Toaster position="top-center" reverseOrder={false} />
+
+      <div className="container">
+        <div>
+          <div>
+            <h1>WELCOME DAD</h1>
+            {/*  */}
+            <div className="formContainer">
+              <form className="form" onSubmit={submitHandler}>
+                <div className="f1">
+                  <div>
+                    <p>Old Payment :</p>
+                    <input
+                      type="number"
+                      value={oldPayment}
+                      onChange={(e) => setOldPayment(e.target.value)}
+                      required
+                    />
+                  </div>
+                  {/*  */}
+                  <div>
+                    <p>Paid Recently :</p>
+                    <input
+                      type="number"
+                      value={paidRecently}
+                      onChange={(e) => setPaidRecently(e.target.value)}
+                      required
+                    />
+                  </div>
+                  {/*  */}
+                  <div>
+                    <div>
+                      <p>Date :</p>
+                      <input
+                        type="date"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                        required
+                      />
+                    </div>
+                    {/*  */}
+                    <div>
+                      <p>Nag :</p>
+                      <input
+                        type="number"
+                        value={nag}
+                        onChange={(e) => setNag(e.target.value)}
+                        required
+                      />
+                    </div>
+                  </div>
+                  {/*  */}
+                  <div>
+                    <p>Amount per nag :</p>
+                    <input
+                      type="number"
+                      value={amountPerNag}
+                      onChange={(e) => setAmountPerNag(e.target.value)}
+                      required
+                    />
+                  </div>
+                </div>
+                {/*  */}
+                <div className="f2">
+                  <div>
+                    <p>Old Empty :</p>
+                    <input
+                      type="number"
+                      value={oldEmpty}
+                      onChange={(e) => setOldEmpty(e.target.value)}
+                      required
+                    />
+                  </div>
+                  {/*  */}
+                  <div>
+                    <div>
+                      <p>Date :</p>
+                      <input
+                        type="date"
+                        value={emptyDate}
+                        onChange={(e) => setEmptyDate(e.target.value)}
+                        required
+                      />
+                    </div>
+                    {/*  */}
+                    <div>
+                      <p>Empty :</p>
+                      <input
+                        type="number"
+                        value={recentEmpty}
+                        onChange={(e) => setRecentEmpty(e.target.value)}
+                        required
+                      />
+                    </div>
+                  </div>
+                </div>
+                {/*  */}
+                <div className="btns">
+                  <button type="reset" onClick={resetHandler}>
+                    Reset
+                  </button>
+                  {/*  */}
+                  <button type="submit">Submit</button>
+                </div>
+              </form>
+            </div>
+            {/*  */}
+            <div className="output">
+              <textarea
+                readOnly
+                name="output"
+                value={output}
+              ></textarea>
+              <CopyToClipboard
+                text={output}
+                onCopy={() => toast.success(`Copied`)}
+              >
+                <button className="copyBtn">Copy To Clipboard</button>
+              </CopyToClipboard>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
